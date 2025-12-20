c0d909b7b717938ea22d6636ac5fd0847b083e50
דניאל <dniel1754@gmail.com>
2025-12-20 19:07:14 +0200

    112

--- src/Ex2/Index2D_Test.java
+++ src/Ex2/Index2D_Test.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class Index2D_Test {
+    // the first 2 test cheks the get and set function and the 3 test also cheks the get and set with a copy
     @Test
     void testGettersAndConstructor() {
         Index2D a = new Index2D(5, 6);
@@ -17,6 +18,54 @@ class Index2D_Test {
         Index2D p = new Index2D(3, 4);
         assertEquals(3, p.getX(), "X should be 3");
         assertEquals(4, p.getY(), "Y should be 4");
+    }
+    @Test
+    void testCopyConstructor() {
+        Index2D p1 = new Index2D(5, 10);
+        Index2D p2 = new Index2D(p1);
+
+        assertEquals(p1.getX(), p2.getX());
+        assertEquals(p1.getY(), p2.getY());
+        assertNotSame(p1, p2, "Copy constructor should create a new object");
+    }
+    // test for toString function
+    @Test
+    void testToString() {
+        Index2D u = new Index2D(7,8);
+        assertEquals("7,8",u.toString());
+
+    }
+    // test for distance function from - Pythagoras' theorem - the distance from points is sqrt between
+    // addind  squared of both of them
+    @Test
+    void testDistance2D() {
+        Index2D p1 = new Index2D(0, 0);
+        Index2D p2 = new Index2D(3, 4);
+
+        double dist = p1.distance2D(p2);
+        assertEquals(5.0, dist, 0.001);
+
+    }
+    // another test for distance , distance of point to himself equals zero
+    @Test
+    void testDistance2DBetweenhimselfe() {
+        Index2D p2 = new Index2D(3, 4);
+
+        double dist = p2.distance2D(p2);
+        assertEquals(0.0, dist, 0.001);
+
+    }
+    // test for equals function if points are equals
+    @Test
+    void testEquals() {
+        Index2D p1 = new Index2D(5, 5);
+        Index2D p2 = new Index2D(5, 5);
+        Index2D p3 = new Index2D(2, 3);
+        assertTrue(p1.equals(p2));
+        assertTrue(p2.equals(p1));
+        assertFalse(p1.equals(p3));
+
+
     }
 
 
--- src/Ex2/Map.java
+++ src/Ex2/Map.java
@@ -9,6 +9,9 @@ import java.io.Serializable;
  */
 public class Map implements Map2D, Serializable{
 
+
+    private int [][] map;
+
     // edit this class below
 	/**
 	 * Constructs a w*h 2D raster map with an init value v.
@@ -16,83 +19,136 @@ public class Map implements Map2D, Serializable{
 	 * @param h
 	 * @param v
 	 */
-	public Map(int w, int h, int v) {init(w, h, v);}
+	public Map(int w, int h, int v) {
+        init(w, h, v);
+    }
 	/**
 	 * Constructs a square map (size*size).
 	 * @param size
 	 */
-	public Map(int size) {this(size,size, 0);}
+	public Map(int size)
+    {
+        this(size,size, 0);
+    }
 	
 	/**
 	 * Constructs a map from a given 2D array.
 	 * @param data
 	 */
 	public Map(int[][] data) {
-		init(data);
+
+        init(data);
 	}
 	@Override
 	public void init(int w, int h, int v) {
+        if (w != 0 && h != 0) {
+            int [] [] array = new int[w][h];
+            for (int i =0;i<array.length;i++){
+                for (int j =0;j<array[0].length;j++){
+                    array[i][j] = v;
+
+                }
+
+            }
+        }
+
 
 	}
 	@Override
 	public void init(int[][] arr) {
+        if (arr == null || arr.length<1 || arr[0].length< 1){
+            throw new RuntimeException();
+        }
+        int [] [] arrCopy = new int[arr.length][arr[0].length];
+        for (int i =0;i<arrCopy.length;i++){
+            for (int j =0;j<arrCopy[0].length;j++){
+                arrCopy[i][j] = arr[i][j];
+            }
+
+        }
 
 	}
 	@Override
 	public int[][] getMap() {
 		int[][] ans = null;
+        if (this.map == null)
+            return ans;
+        int w = getWidth();
+        int h = getHeight();
+        int [] [] copy = new int[w][h];
+        for (int i =0; i<copy.length; i++){
+            for (int j = 0;j<copy[0].length;j++){
+                copy[i][j] = this.map[i][j];
 
-		return ans;
+            }
+        }
+		return copy;
 	}
 	@Override
 	public int getWidth() {
         int ans = -1;
+        if (this.map == null)
+            return ans;
 
-        return ans;
+
+        return this.map.length;
     }
 	@Override
 	public int getHeight() {
         int ans = -1;
+        if (this.map == null)
+            return ans;
 
-        return ans;
+        return this.map[0].length;
     }
 	@Override
 	public int getPixel(int x, int y) {
         int ans = -1;
+        if (x<0 || y<0)
+            return ans;
 
-        return ans;
+        return this.map[x][y];
     }
 	@Override
 	public int getPixel(Pixel2D p) {
         int ans = -1;
+        if (p.getX()<0 || p.getY()<0)
+            return ans;
 
-        return ans;
+        return getPixel(p.getX(),p.getY());
 	}
 	@Override
 	public void setPixel(int x, int y, int v) {
+        map[x][y] = v;
 
     }
 	@Override
 	public void setPixel(Pixel2D p, int v) {
+        setPixel(p.getX(),p.getY(),v);
 
 	}
 
     @Override
     public boolean isInside(Pixel2D p) {
         boolean ans = true;
-
-        return ans;
+        int x = p.getX();
+        int y = p.getY();
+        int h = getHeight();
+        int w = getWidth();
+        return (x>0 && x<w && y>0 && y>h);
     }
 
     @Override
     public boolean sameDimensions(Map2D p) {
         boolean ans = false;
-
+        if (this.getWidth() == p.getWidth() && this.getHeight() == p.getHeight())
+            ans = true;
         return ans;
     }
 
     @Override
     public void addMap2D(Map2D p) {
+
 
     }
 
